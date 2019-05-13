f37ee2a8e6825bcd159da6a04c3665d2864f290d
Zac Smith <zrs3141592@gmail.com>
2019-05-13 15:38:11 -0600

    finished article.js

--- Article/Article.js
+++ Article/Article.js
@@ -1,20 +1,20 @@
 // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 class Article {
-  constructor(domElement) {
+  constructor(article) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.article = article;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = article.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textConnect = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click', () => this.expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.article.classList.toggle('article-open');
   }
 }
 
@@ -26,4 +26,8 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.article');
+// console.log(article);
+
+articles.forEach(article => new Article(article));
+

5829b0fb4497e54bfa53696bf46e4d62c9375ea1
Zac Smith <zrs3141592@gmail.com>
2019-05-13 15:59:34 -0600

    correct typo

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,7 +1,7 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
-  event.classList.toggle('.menu-open');
+  event.classList.toggle('.menu--open');
 }
 
 // Start Here: Create a reference to the ".menu" class
